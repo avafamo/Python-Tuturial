a9b576afd9dda2c18825a134d9505037d692adbf
Ava <ava.famo@gmail.com>
2025-03-31 23:46:16 +0330

    Loop

---
+++ Loop.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+listOfNumbers = [23, 54, 67, 89, 34, 9]
+
+# for number in listOfNumbers:
+#     print(number * 2)
+
+
+myName = "Ava"
+
+# for character in myName:
+#     print(character)
+
+rangeOfNumbers = range(1, 10)
+
+for num in rangeOfNumbers:
+    print(num * num)
